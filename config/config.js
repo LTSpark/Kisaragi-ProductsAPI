19a347e51491cf578ded66fc4f9d5219e96da3cd
Leonardo Torres Briones <dleonardotb2001@gmail.com>
2021-12-17 01:51:32 -0500

    fix: config development - production

--- config/config.js
+++ config/config.js
@@ -5,17 +5,12 @@ const { DATABASE_URL } = process.env;
 console.log(DATABASE_URL, "DATABASE_URL");
 
 const DB_USERNAME = DATABASE_URL.split("//")[1].split(":")[0];
-console.log(DB_USERNAME, "DB_USERNAME");
 const DB_PASSWORD = DATABASE_URL.split("//")[1].split(":")[1].split("@")[0];
-console.log(DB_PASSWORD, "DB_PASSWORD");
 const DB_DATABASE = DATABASE_URL.split("//")[1].split("@")[1].split("/")[1];
-console.log(DB_DATABASE, "DB_DATABASE");
 const DB_HOST = DATABASE_URL.split("//")[1]
   .split("@")[1]
   .split("/")[0]
   .split(":")[0];
-
-console.log(DB_HOST, "DB_HOST");
 
 module.exports = {
   development: {
@@ -24,12 +19,6 @@ module.exports = {
     database: DB_DATABASE,
     host: DB_HOST,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
   production: {
     username: DB_USERNAME,
